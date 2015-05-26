81949aa2c4244da863394d00bd4d7e05ea048c31
SunPHM <pengyang823@gmail.com>
2015-05-26 14:51:26 -0400

    adding logistic regression fusion and automatic weight computation

--- fusion.py
+++ fusion.py
@@ -2,7 +2,8 @@ import os
 import sys
 from sklearn import linear_model
 
-a = 0.8
+word = "bass"
+a = 0.6
 
 def readResults(filename):
     	res_map = {}
@@ -37,6 +38,21 @@ def getCMatrix(rmap):
 				m10 += 1
 	print m00, m01, m10, m11
 	return m00, m01, m10, m11
+
+def compute_weight(vimap, vtmap):
+	a1 = 0
+	a2 = 0
+	s = 0
+	for k in vimap:
+		if k not in vtmap: continue
+		s += 1
+		if vimap[k][0] == vimap[k][3]:
+			a1 += 1
+		if vtmap[k][0] == vtmap[k][3]:
+			a2 += 1
+	p1 = float(a1) / float(s)
+	p2 = float(a2) / float(s)
+	return p1 / (p1 + p2) 
 
 def maxfusion(imap, tmap):
 	mmap = {}
@@ -79,15 +95,78 @@ def linearfusion(imap, tmap):
         return lmap
 
 
-def logfusion():
-	print ""
+def logfusion(vimap, vtmap, imap, tmap):
+	lr = linear_model.LogisticRegression()
+	# read features and labels
+	X = []
+	y = []
+	for k in vimap:
+		if k not in vtmap: continue
+		p = []
+		p.append(vimap[k][0])
+		p.append(vimap[k][1])
+		p.append(vimap[k][2])
+		p.append(vtmap[k][0])
+		p.append(vtmap[k][1])
+		p.append(vtmap[k][2])
+		X.append(p)
+		y.append(vimap[k][3])
+	# train logistic regression
+	lr.fit(X, y)
+	# run the lr classifier on testing data and store the results		
+	rmap = {}
+	keys = []
+	features = []
+	truth = []
+	for k in imap:
+		if k not in tmap: continue
+		feature = []
+		feature.append(imap[k][0])
+		feature.append(imap[k][1])
+		feature.append(imap[k][2])
+		feature.append(tmap[k][0])
+		feature.append(tmap[k][1])
+		feature.append(tmap[k][2])
+		features.append(feature)
+		keys.append(k)
+		truth.append(imap[k][3])
+	
+	labels = lr.predict(features)
+	probs = lr.predict_proba(features)
+	for i in range(len(keys)):
+		tup = []
+		tup.append(labels[i])
+		tup.append(probs[i][0])
+		tup.append(probs[i][1])
+		tup.append(truth[i])
+		rmap[keys[i]] = tup
+			
+	#getCMatrix(rmap)
+	#print "logistic regression is done"
+	return rmap 
 
+if len(sys.argv) > 1:
+	word = sys.argv[1]
+if len(sys.argv) > 2:
+	a = float(sys.argv[2])
+print "processing word", word
+img_test = "data/ed/" + word + "/img_test_results.txt"
+txt_test = "data/ed/" + word + "/txt_test_results.txt"
+img_valid = "data/ed/" + word + "/img_valid_results.txt"
+txt_valid = "data/ed/" + word + "/txt_valid_results.txt"
+
+print "loading files"
+vimap = readResults(img_valid)
+vtmap = readResults(txt_valid)
+imap = readResults(img_test)
+tmap = readResults(txt_test)
+
+a = compute_weight(vimap, vtmap)
+print "weight = ", a
 
 print "image"
-imap = readResults(sys.argv[1])
 getCMatrix(imap)
 print "text"
-tmap = readResults(sys.argv[2])
 getCMatrix(tmap)
 print "max-rule fusion"
 mmap = maxfusion(imap, tmap)
@@ -95,3 +174,6 @@ getCMatrix(mmap)
 print "linear-rule fusion"
 lmap = linearfusion(imap, tmap)
 getCMatrix(lmap)
+print "logistice regression fusion"
+lrmap = logfusion(vimap, vtmap, imap, tmap)
+getCMatrix(lrmap)
