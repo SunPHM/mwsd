b04744ae5546be7e2421c49a29ceb1e3068b8f0c
Yang Peng <pengyang823@gmail.com>
2015-06-09 15:32:21 -0400

    try pos tagging and named entity recognition on training text

--- g-fusion/txt-parse.py
+++ g-fusion/txt-parse.py
@@ -1,16 +1,34 @@
 import nltk, re, pprint
 import os
 import sys
+import codecs
 
-def main():
-	s = "On World Oceans Day, explore their beauty with Google Maps"
-	ws = nltk.word_tokenize(s)
-	ps = nltk.pos_tag(ws)
-	#ns = nltk.ne_chunk(ps, binary=True)
-	ns = nltk.ne_chunk(ps)
-	print ws
-	print ps
-	print ns
+def read_file(filename):
+	print "reading file"
+	f = open(filename,'r')
+	docs = []
+	for line in f:
+		#print line
+		docs.append(unicode(line, errors="ignore"))
+	return docs	
 
+def extractNE(docs):# extract named entities
+	for s in docs:
+		ws = nltk.word_tokenize(s)
+		ps = nltk.pos_tag(ws)
+		ns = nltk.ne_chunk(ps)
+		#ns = nltk.ne_chunk(ps, binary=True)
+		#print ps
+		#print ns
+		for x in ps:
+			print x
+		#	if type(x) is nltk.tree.Tree:
+		#		print s, x
+		#		print ps	
+
+def main(filename):
+	docs = read_file(filename)
+	extractNE(docs)
+	
 if __name__ == "__main__":
-	main()
+	main("../data/ed/bass/train/info.txt")
