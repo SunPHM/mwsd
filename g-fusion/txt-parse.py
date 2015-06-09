209df8f3b14f4050da4e7cf576ac215444fe3638
SunPHM <pengyang823@gmail.com>
2015-06-08 22:16:40 -0400

    minor adding

--- g-fusion/txt-parse.py
+++ g-fusion/txt-parse.py
@@ -6,8 +6,11 @@ def main():
 	s = "On World Oceans Day, explore their beauty with Google Maps"
 	ws = nltk.word_tokenize(s)
 	ps = nltk.pos_tag(ws)
+	#ns = nltk.ne_chunk(ps, binary=True)
+	ns = nltk.ne_chunk(ps)
 	print ws
 	print ps
+	print ns
 
 if __name__ == "__main__":
 	main()
